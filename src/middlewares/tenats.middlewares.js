0f9c765c7945c6a974712f9bb620e0f9840f0a74
gui24xr <guillermoxr24@gmail.com>
2025-11-10 20:57:08 -0300

    Fuera el hardcoding

--- src/middlewares/tenats.middlewares.js
+++ src/middlewares/tenats.middlewares.js
@@ -14,13 +14,13 @@ function checkTenantAndAuth(req,res,next) {
 
         const tenant = DOMAINS_MAP[host]
 
-        /*
+        
         if (!tenant) {
             return res.status(401).json({ error: 'tenant not found!' });
         }
-            */   
+              
 
-        req.tenant = tenant || 'hotdog' -->> //El OPr harcodeado para ngrok x si no viene ningun dominio
+        req.tenant = tenant 
         next()
     }catch(error){
         res.status(500).json({ message: error.message })
